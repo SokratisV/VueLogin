090537f32d2b555d9a20033125e699dd3ba25429
Sokratis Vogiatzakis <sokvogiatzakis@gmail.com>
2018-02-19 21:55:57 +0200

    VueLogin

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <br>
+    <md-card class="myclass">
+      <md-card-header>
+        <div class="md-title">
+          Login with Vue-material.
+        </div>
+      </md-card-header>
+      <md-card-content>
+        <md-field>
+          <label>Username</label>
+          <md-input v-model="user"/>
+        </md-field>
+        <md-field>
+          <label>Email</label>
+          <md-input v-model="email"/>
+        </md-field>
+      </md-card-content>
+    </md-card>
+    <md-button class="md-raised md-primary" v-on:click="login()">Login</md-button>
+    <md-button class="md-raised md-accent" v-on:click="signup()">Sing up</md-button>
+    <!--<users></users>-->
+  </div>
+</template>
+
+<script>
+  import test from './components/test'
+  import users from './components/users'
+
+  export default {
+    name: 'App',
+    components: {
+      // test, users
+    },
+    methods: {
+      login: function () {
+        alert('logging in');
+      },
+      signup: function () {
+        alert('signing up');
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    text-align: center;
+    margin-top: 60px;
+    margin-left: 30px;
+    margin-right: 30px;
+  }
+
+  .md-primary {
+    background-color: #448aff;
+  }
+
+  .md-accent {
+    background-color: #ff5252;
+  }
+  .myclass{
+    background-color: #42b883;
+    /*background-color: #35495e;*/
+  }
+  .myclass{
+    margin-right: 35%;
+    margin-left: 35%;
+  }
+</style>
---
+++ src/components/users.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="users">
+    <h1>Login</h1>
+    <form v-on:submit="login">
+      <input type="text" v-model="newUser.username" placeholder="Enter Username">
+      <br>
+      <input type="text" v-model="newUser.email" placeholder="Enter Email">
+      <br>
+      <input type="submit" value="Login">
+    </form>
+    <!--<ul>-->
+      <!--<li v-for="user in users">-->
+      <!--</li>-->
+    <!--</ul>-->
+  </div>
+</template>
+
+<script>
+  export default {
+    username: 'users',
+    data() {
+      return {
+        newUser: {},
+        users: []
+      }
+    },
+    methods: {
+      login: function (e) {
+        e.preventDefault();
+        for (let i = 0; i < this.users.length; i++) {
+          if (this.newUser.username === this.users[i].username && this.newUser.email === this.users[i].phone) {
+            alert('login successful');
+
+            return true;
+          }
+        }
+        alert('login failed');
+        return false;
+      }
+    },
+    created: function () {
+      this.$http.get('https://jsonplaceholder.typicode.com/users')
+        .then(function (response) {
+          this.users = response.data;
+          console.log(response.data);
+        });
+    }
+  }
+
+</script>
+
+<style scoped>
+  .contacted {
+    text-decoration: line-through;
+  }
+
+</style>
